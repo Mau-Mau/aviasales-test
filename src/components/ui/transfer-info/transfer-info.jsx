ef62cc940ae67e66d71f277117d269cbf3b0a984
Mau-Mau <nina.mau.nsk@gmail.com>
2022-06-30 12:17:56 +0700

    Creates mocks for tickets. Refactors ticketCard

--- src/components/ui/transfer-info/transfer-info.jsx
+++ src/components/ui/transfer-info/transfer-info.jsx
@@ -2,18 +2,32 @@ import React from "react";
 
 function TransferInfo() {
   return (
-    <table class="transfer-info">
-      <tr class="transfer-info__item">
-        <th>MOW - HKT</th>
-        <th>В пути</th>
-        <th>2 пересадки</th>
-      </tr>
-      <tr class="transfer-info__item">
-        <td>10:45 - 08:00</td>
-        <td>21ч 15м</td>
-        <td>HKG, JNB</td>
-      </tr>
-    </table>
+    <div class="transfer-info">
+      <table class="transfer-info__flight">
+        <tr class="transfer-info__heading">
+          <th>MOW - HKT</th>
+          <th>В пути</th>
+          <th>2 пересадки</th>
+        </tr>
+        <tr class="transfer-info__description">
+          <td>10:45 - 08:00</td>
+          <td>21ч 15м</td>
+          <td>HKG, JNB</td>
+        </tr>
+      </table>
+      <table class="transfer-info__flight">
+        <tr class="transfer-info__heading">
+          <th>HKT - MOW</th>
+          <th>В пути</th>
+          <th>1 пересадка</th>
+        </tr>
+        <tr class="transfer-info__description">
+          <td>11:20 - 00:50</td>
+          <td>13ч 30м</td>
+          <td>HKG</td>
+        </tr>
+      </table>
+    </div>
   );
 }
 
